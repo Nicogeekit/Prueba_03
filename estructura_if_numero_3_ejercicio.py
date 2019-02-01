a8195607208afb8390abd4b7394ee5453b48db48
Nicolasgeekit <nicolas.salazar.it@gmail.com>
2019-02-01 17:03:48 -0300

    Probando el commit

---
+++ estructura_if_numero_3_ejercicio.py
@@ -0,0 +1,30 @@
+
+
+numero_correcto = 10
+
+numero_del_usuario = int(input("Diga su numero: "))
+
+
+if numero_del_usuario == numero_correcto:
+    print ("Ganaste, acertaste el numero")
+
+else:
+ numero_del_usuario = int(input("diga su numero nuevamente: "))
+
+if numero_del_usuario == numero_correcto:
+    print ("Ganaste, acertaste el numero")
+
+else:
+    numero_del_usuario = int (input("Diga si numero nuevamente le quedan 2 intentos: "))
+
+if numero_del_usuario == numero_correcto:
+    print ("Ganaste, acertaste el numero")
+
+else:
+    numero_del_usuario = int (input("Diga su numero nuevamente, le quedan 1 intentos: "))
+
+if numero_del_usuario == numero_correcto:
+    print ("Ganaste, acertaste el numero")
+
+else:
+    print ("Usaste todos los intentos, fallaste.")
